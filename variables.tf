baf63c24270a91dd935f0709705e0c1ca1f785da
Chris Wright <chris@dxw.com>
2025-09-08 12:13:27 +0100

    Add default resource group
    * Creates a resource group, where the resources will be created
    * Conditionally specify an existing resource group to use that instead

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "environment" {
+  description = "Environment name. Will be used along with `project_name` as a prefix for all resources."
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name. Will be used along with `environment` as a prefix for all resources."
+  type        = string
+}
+
+variable "azure_location" {
+  description = "Azure location in which to launch resources."
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to be applied to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "existing_resource_group" {
+  description = "Conditionally launch resources into an existing resource group. Specifying this will NOT create a resource group."
+  type        = string
+  default     = ""
+}
